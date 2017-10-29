73ee9083e5542a582b331326af9ce4481c8754ba
Павел <bavenka@gmail.com>
2017-10-29 17:40:59 +0300

    added categories/subcategories work logic

--- app/validators/categoryValidator.js
+++ app/validators/categoryValidator.js
@@ -3,6 +3,6 @@ import Joi from 'joi';
 export default {
     body: {
         name: Joi.string().required(),
-        parent_id: Joi.number().integer(),
+        parent_id: Joi.number().integer().min(1),
     },
 };
--- app/validators/userValidator.js
+++ app/validators/userValidator.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 export default {
     body: {
-        name: Joi.string().required(),
-        lastName: Joi.string().required(),
+        name: Joi.string().alphanum().required(),
+        lastName: Joi.string().alphanum().required(),
         email: Joi.string().email().required(),
         password: Joi.string().required(),
     },
